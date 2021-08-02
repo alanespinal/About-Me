465419bd0a56381e13f23e381f8e04e30c12c892
mralanman <>
2021-08-02 14:22:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function caption1(){
+  document.getElementById("caption1").innerHTML = "p5.js";
+}
+function caption2(){
+  document.getElementById("caption2").innerHTML = "Javascript";
+}
+function caption3(){
+  document.getElementById("caption3").innerHTML = "CSS";
+}
+function caption4(){
+  document.getElementById("caption4").innerHTML = "HTML";
+}
+function caption5(){
+  document.getElementById("Atleti").innerHTML = "My favorite sport is football. I support Spanish football club Atletico de Madrid, and my preferred position is Right-back.";
+}
+function caption6(){
+  document.getElementById("craft").innerHTML = "One of my favorite videogames to play is Minecraft, and I enjoy speedrunning it. My personal best time is (unofficially) top 100 in the world.";
+}
+function caption7(){
+  document.getElementById("chess").innerHTML = "I enjoy the game of chess, I have been playing it for about a year and a half now. I also enjoy watching chess content creators such as GothamChess and Daniel Naroditsky.";
+}
+function caption8(){
+  document.getElementById("Star").innerHTML = "I am a huge Star Wars fan (not of the sequels). My favorite Star Wars movies are Revenge of the Sith and The Empire Strikes Back.";
+}
+function captionLeave(){
+  document.getElementsByClassName("caption").value = "";
+}
